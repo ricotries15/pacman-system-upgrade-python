990bafef01160754c721786d50e2c6f3b6330672
Anthony Benitez <anthony.bebor@gmail.com>
2021-11-06 21:46:47 -0400

    Add code to check network connectivity

--- pacman-system-upgrade.py
+++ pacman-system-upgrade.py
@@ -9,3 +9,13 @@ if (os.getuid() != 0):
 if (os.path.isfile("/etc/arch-release") == False):
     print("system is not arch linux!")
     sys.exit(1)
+
+net_check = subprocess.Popen(
+    "ping -c 1 archlinux.org",
+    shell=True,
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL)
+
+if (net_check.wait() != 0):
+    print("system does not have network connectivity!")
+    sys.exit(1)
