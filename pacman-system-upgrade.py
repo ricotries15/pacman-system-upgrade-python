f344a41c6036584fc761a2cada1f5a4e897bf30d
Anthony Benitez <anthony.bebor@gmail.com>
2021-11-06 21:46:27 -0400

    Add code to confirm host is Arch Linux

--- pacman-system-upgrade.py
+++ pacman-system-upgrade.py
@@ -5,3 +5,7 @@ import os, subprocess
 if (os.getuid() != 0):
     print("script requires elevated privileges!")
     sys.exit(1)
+
+if (os.path.isfile("/etc/arch-release") == False):
+    print("system is not arch linux!")
+    sys.exit(1)
